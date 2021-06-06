b6f5cc3ab4de3e2734ebd37900de154e667a4fd7
marigut <mari.gutierrez96@yahoo.com>
2021-06-06 02:27:57 -0500

    notes created with unique IDs

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,9 +1,21 @@
-//const router = require("express").Router();
+const router = require("express").Router();
+const { createNewNote, validateNote, } = require("../../lib/notes");
+const { notes } = require("../../data/db");
 
-//const { notes } = require("../../db/db.json")
 
-//router.get('/api/notes', (req, res) => {
-  //res.json(notes);
-//});
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});
 
-//module.exports = router;
+router.post("/notes", (req, res) => {
+  
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const { notes } = require('./db/db');
-//const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 //const htmlRoutes = require('./routes/htmlRoutes');
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
-});
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+//app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
