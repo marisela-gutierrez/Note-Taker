02e0c0cfdbcc3e5cb983d0c676666aa1de2a3079
marigut <mari.gutierrez96@yahoo.com>
2021-06-05 21:50:57 -0500

    heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const { notes } = require('./db/db');
@@ -10,6 +11,6 @@ app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
