9fbae5345a66ba1fda7a9f3b6a4ebcaedd22b7a7
marigut <mari.gutierrez96@yahoo.com>
2021-06-05 21:49:09 -0500

    Add Heroku

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,9 @@
+//const router = require("express").Router();
+
+//const { notes } = require("../../db/db.json")
+
+//router.get('/api/notes', (req, res) => {
+  //res.json(notes);
+//});
+
+//module.exports = router;
--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
+
 const app = express();
+
+const { notes } = require('./db/db');
+//const apiRoutes = require('./routes/apiRoutes');
+//const htmlRoutes = require('./routes/htmlRoutes');
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
